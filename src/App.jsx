4c3a0c9ecacd870446df3bbbca15f62f0d8e0d78
vKS-Rajput <k8659653@gmail.com>
2025-04-14 04:33:03 +0530

    Error Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ function App() {
 
   return (
     <>
-     <h2> Welcome</h2>
-     <button>Subscribe</button>
+     <h2 className='text-5xl text-red-700 font-bold'> Hello Suman </h2>
+     <button className='text-[50px] text-yellow-500 font-bold'>Get Back to Work, You Don't have Time</button>
+
     </>
   )
 }
