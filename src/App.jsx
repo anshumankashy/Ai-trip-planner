cfd7dded86c1fb222b99ab54d774a1833f93c763
Anshumankashy <anshumankashyap773@gmail.com>
2025-04-14 06:38:48 +0530

    issue in main.jsx (header )

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <>
-     <h2 className='text-5xl text-red-700 font-bold'> Hello Suman </h2>
-     <button className='text-[50px] text-yellow-500 font-bold'>Get Back to Work, You Don't have Time</button>
-
+       {/* Hero  */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path:'/create-trip',
+     element:<create-trip/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Header/>
+    <RouterProvider router={router} />
+  </StrictMode>
 )
