04d237db3f812184fde18b6c2a82e5073b9ac83a
Anshumankashy <anshumankashyap773@gmail.com>
2025-04-14 21:45:59 +0530

    second page

---
+++ src/constants/options.jsx
@@ -0,0 +1,55 @@
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A solo traveles in exploration',
+        icon:'✈️',
+        people:'1'    
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc:'Two travels in tendem',
+        icon:'👩‍❤️‍👨',
+        people:'2 Peaople'    
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:'🏠❤️',
+        people:'3 to 5 People'    
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'A bunch of thrill-seeks',
+        icon:'🏕️',
+        people:'5 to People'    
+    },
+    
+]
+
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:'Stay conscious of costs',
+        icon:'💰',
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:'💵',
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:'💸',
+    },
+]
+
+export const AI_PROMPT='Generate Trvel Plan for Location: {location}'
+
