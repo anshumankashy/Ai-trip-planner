9e412c80d0d7f20635d32122cd3a1fb1a18096f9
vKS-Rajput <k8659653@gmail.com>
2025-04-14 22:15:47 +0530

    01:08:00 Time

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/ui/Custom/Header'
 import Hero from './components/ui/Custom/Hero'
 import CreateTrip from './create-trip'
+import { Toaster } from './components/ui/sonner'
 
 function App() {
   return (
     <>
       <Header />
-
+      <Toaster/>
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/create-trip" element={<CreateTrip />} />
---
+++ src/service/AiModel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AiModel = () => {
+  return (
+    <div>AiModel</div>
+  )
+}
+
+export default AiModel
