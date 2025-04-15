f4ecc1a6b8b0d57b260b98ec85b76754bb6890bc
Anshumankashy <anshumankashyap773@gmail.com>
2025-04-16 00:40:51 +0530

    kuchh to garab hai daya

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -5,10 +5,17 @@ import { Button } from '@/components/ui/button';
 function Hotels({ trip }) {
   return (
      <div>
-          <img src="/placeholder.jpg" className='h-[340px] w-full object-cover rounded-lg' />
-    
+        <h2 className='font-bold text-xl mt-5'>Hotel Recommendation</h2>
+
+        <div className='grid-bold cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {trip?.tripData?.hotel?.map((item,index)=>(
+               <div>
+                  <img src="/placeholder.jpg" className='h-[340px] w-full object-cover rounded-lg' />
+               </div>
+            ))}
         </div>
-      )
-    }
+    </div>
+  )
+}
 
 export default Hotels;
