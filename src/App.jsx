c98390778fca97824d0edd042538e126cf4404af
Anshumankashy <anshumankashyap773@gmail.com>
2025-04-15 17:00:14 +0530

    AI Trip Generation Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Header from './components/ui/Custom/Header'
 import Hero from './components/ui/Custom/Hero'
 import CreateTrip from './create-trip'
 import { Toaster } from './components/ui/sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function App() {
   return (
     <>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+
       <Header />
       <Toaster/>
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/create-trip" element={<CreateTrip />} />
       </Routes>
+      </GoogleOAuthProvider>
     </>
   )
 }
